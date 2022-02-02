ebf37b4d24782a5d674bec510aed282ddbe6c383
marcovdss <wintermarck@outlook.com>
2022-02-02 15:47:53 -0300

    Bloco 4, Intro a JS

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica/dia-01-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+let age = 20;
+let name = "Hulk";
+console.log(age);
+console.log(name);
+///
+const myName = "Marcos";
+const birthCity = "Brasilia";
+let birthYear = 1997;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "Sao Paulo";
+console.log(birthCity);
+//Erro devido a tentativa de alteração em uma constante
